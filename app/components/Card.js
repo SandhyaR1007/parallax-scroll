fc4dfa749caee43fa6bc334adfd6291f91c8c6c6
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2025-01-11 13:40:04 +0530

    update: scaling effect for images

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,9 +1,45 @@
+"use client";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
+
 const Card = ({ title, description, color }) => {
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "start start"],
+  });
+  const scale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  console.log(scrollYProgress);
   return (
-    <div className="cardContainer">
-      <div className="card" style={{ backgroundColor: color }}>
-        <h3>{title}</h3>
-        <p>{description}</p>
+    <div className="cardContainer" ref={containerRef}>
+      <div
+        className="card flex justify-between items-center gap-5 p-5"
+        style={{ backgroundColor: color }}
+      >
+        <div className="flex-1">
+          {" "}
+          <h3 className="text-4xl py-3 font-semibold">{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="imageContainer">
+          <motion.div
+            className="flex-1 inner"
+            style={{
+              scale,
+            }}
+          >
+            <Image
+              className="rounded-xl object-cover"
+              alt="image"
+              src={
+                "https://t4.ftcdn.net/jpg/05/62/02/41/360_F_562024161_tGM4lFlnO0OczLYHFFuNNdMUTG9ekHxb.jpg"
+              }
+              width={500}
+              height={500}
+            ></Image>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
